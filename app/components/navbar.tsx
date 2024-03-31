ef793e0ceefa744cc1f22b62022e19616107fb29
Tejas-0001 <202111080@diu.iiitvadodara.ac.in>
2024-03-31 14:57:05 +0530

    navbar responsive initialized

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -4,40 +4,37 @@ import React from 'react'
 const navbar = () => {
     return (
         <>
-        <div className="navbar bg-base-100">
-            <div className="navbar-start">
-                <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+            <div className="navbar bg-base-100">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </div>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                            <li><Link href="../cricket">Cricket</Link></li>
+                            <li><Link href="../football">Football</Link></li>
+                            <li><Link href="../basketball">Basketball</Link></li>
+                            <li><Link href="../volleyball">Volleyball</Link></li>
+                            <li><Link href="../tabletennis">Table Tennis</Link></li>
+                            <li><Link href="../sponsors">Sponsors</Link></li>
+                        </ul>
                     </div>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <a>Parent</a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
+                    <a className="btn btn-ghost text-xl">OLYMPUS</a>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        <li><Link href="../cricket">Cricket</Link></li>
+                        <li><Link href="../football">Football</Link></li>
+                        <li><Link href="../basketball">Basketball</Link></li>
+                        <li><Link href="../volleyball">Volleyball</Link></li>
+                        <li><Link href="../tabletennis">Table Tennis</Link></li>
+                        <li><Link href="../sponsors">Sponsors</Link></li>
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">OLYMPUS</a>
+                <div className="navbar-end">
+                    <Link href="../scoreboard" className="btn">Scoreboard</Link>
+                </div>
             </div>
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                    <li><Link href="../cricket">Cricket</Link></li>
-                    <li><Link href="../football">Football</Link></li>
-                    <li><Link href="../basketball">Basketball</Link></li>
-                    <li><Link href="../volleyball">Volleyball</Link></li>
-                    <li><Link href="../tabletennis">Table Tennis</Link></li>
-                    <li><Link href="../sponsors">Sponsors</Link></li>
-                </ul>
-            </div>
-            <div className="navbar-end">
-                <Link href="../scoreboard" className="btn">Scoreboard</Link>
-            </div>
-        </div>
         </>
     )
 }
