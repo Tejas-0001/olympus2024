0a2bc6d0e9f96318946249fd650b9264a856b217
Tejas-0001 <202111080@diu.iiitvadodara.ac.in>
2024-03-31 15:00:40 +0530

    update

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -11,28 +11,28 @@ const navbar = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </div>
                         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><Link href="../cricket">Cricket</Link></li>
-                            <li><Link href="../football">Football</Link></li>
-                            <li><Link href="../basketball">Basketball</Link></li>
-                            <li><Link href="../volleyball">Volleyball</Link></li>
-                            <li><Link href="../tabletennis">Table Tennis</Link></li>
-                            <li><Link href="../sponsors">Sponsors</Link></li>
+                            <li><Link href="/cricket">Cricket</Link></li>
+                            <li><Link href="/football">Football</Link></li>
+                            <li><Link href="/basketball">Basketball</Link></li>
+                            <li><Link href="/volleyball">Volleyball</Link></li>
+                            <li><Link href="/tabletennis">Table Tennis</Link></li>
+                            <li><Link href="/sponsors">Sponsors</Link></li>
                         </ul>
                     </div>
-                    <a className="btn btn-ghost text-xl">OLYMPUS</a>
+                    <Link href="/" className="btn btn-ghost text-xl">OLYMPUS</Link>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
-                        <li><Link href="../cricket">Cricket</Link></li>
-                        <li><Link href="../football">Football</Link></li>
-                        <li><Link href="../basketball">Basketball</Link></li>
-                        <li><Link href="../volleyball">Volleyball</Link></li>
-                        <li><Link href="../tabletennis">Table Tennis</Link></li>
-                        <li><Link href="../sponsors">Sponsors</Link></li>
+                        <li><Link href="/cricket">Cricket</Link></li>
+                        <li><Link href="/football">Football</Link></li>
+                        <li><Link href="/basketball">Basketball</Link></li>
+                        <li><Link href="/volleyball">Volleyball</Link></li>
+                        <li><Link href="/tabletennis">Table Tennis</Link></li>
+                        <li><Link href="/sponsors">Sponsors</Link></li>
                     </ul>
                 </div>
                 <div className="navbar-end">
-                    <Link href="../scoreboard" className="btn">Scoreboard</Link>
+                    <Link href="/scoreboard" className="btn">Scoreboard</Link>
                 </div>
             </div>
         </>
