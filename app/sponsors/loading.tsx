d3cbb5fb1c1d02d3c5b8965d90e7df7a193a98e7
Tejas-0001 <202111080@diu.iiitvadodara.ac.in>
2024-04-06 02:38:48 +0530

    day updated

--- app/sponsors/loading.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const loading = () => {
-  return (
-    <span className="loading h-screen content-center justify-center align-middle loading-dots loading-lg"></span>
-  )
-}
-
-export default loading
